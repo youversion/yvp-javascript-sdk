ca729075cc761ff060d233f359b8547e5de4ed13
David Fedor <david.fedor@youversion.com>
2025-07-01 17:59:28 -0500

    starter swift code

---
+++ swift/BibleVersionPure.swift
@@ -0,0 +1,273 @@
+import Foundation
+
+@MainActor
+public class BibleVersion: ObservableObject {
+    public let id: Int  // version identifier, e.g. 111 for NIV
+
+    private var metadata: BibleVersionData?
+
+    // MARK: - Init and loading
+    public init(_ id: Int) {
+        self.id = id
+    }
+
+    public static func findByLanguage(_ lang: String? = nil) async -> [BibleVersionOverview] {
+        do {
+            return try await BibleVersionAPIs.versions(forLanguageTag: lang)
+        } catch {
+            print("findByLanguage error: \(error.localizedDescription)")
+            return []
+        }
+    }
+
+    // MARK: - Misc
+
+    public func isValidUSFMBookName(_ name: String) -> String? {
+        guard let metadata else {
+            return nil
+        }
+        let usfm = name.uppercased()
+        for usfmCode in metadata.books.map(\.usfm) where usfmCode == usfm {
+            return usfmCode
+        }
+        return nil
+    }
+
+    // Example: "https://www.bible.com/bible/111/1SA.3.10.NIV"
+    public func shareUrl(reference: BibleReference) -> URL? {
+        let prefix = "https://www.bible.com/bible/\(id)/"
+        var urlString = ""
+
+        guard let book = reference.bookUSFM else {
+            return nil
+        }
+        if reference.isRange {
+            urlString = "\(prefix)\(book).\(reference.chapterStart).\(reference.verseStart)-\(reference.verseEnd).\(abbreviation ?? String(id))"
+        } else {
+            urlString = "\(prefix)\(book).\(reference.chapterStart).\(reference.verseStart).\(abbreviation ?? String(id))"
+        }
+
+        return URL(string: urlString)
+    }
+
+    // MARK: - Formatting
+    public func formatWithVersion(_ reference: BibleReference) -> String {
+        let base = format(reference)
+        if let abbreviation = metadata?.localizedAbbreviation ?? metadata?.abbreviation {
+            if metadata?.language?.textDirection == "rtl" {
+                return "\(abbreviation) \(base)"
+            } else {
+                return "\(base) \(abbreviation)"
+            }
+        }
+        return base
+    }
+
+    public func format(_ reference: BibleReference) -> String {
+        var chunks = formatWorker(reference)
+        if metadata?.language?.textDirection == "rtl" {
+            chunks.reverse()
+        }
+        return chunks.joined()
+    }
+
+    private func formatWorker(_ reference: BibleReference) -> [String] {
+        // for convenience:
+        let b = reference.bookUSFM
+        let c = reference.chapterStart
+        let v = reference.verseStart
+        let c2 = reference.chapterEnd
+        let v2 = reference.verseEnd
+
+        let bcPart1 = bookName(b) ?? ""
+        let bcPart2: String
+        let bcPart3: String
+        var csep = ":"
+
+        if numberOfChaptersInBook(b) == 1 {
+            csep = " "
+            bcPart2 = ""
+            bcPart3 = ""
+        } else {
+            bcPart2 = " "
+            bcPart3 = String(c)
+        }
+        if (v == 0) ||
+            ((v <= 1) && (v2 == 0)) {  // previously, we also looked here at numVersesIn c2, to merge. Removed for now.
+            if c == c2 {
+                return [bcPart1, bcPart2, bcPart3]
+            }
+            return [bcPart1, bcPart2, bcPart3, "-", String(c2)]
+        } else if c != c2 {
+            var vNotZero = v
+            if vNotZero == 0 {
+                vNotZero = 1
+            }
+            var v2NotZero = v2
+            if v2NotZero == 0 {
+                v2NotZero = 999  // TODO fix this hack. Previously, this set it to: numVersesIn(b: b, c: c2)
+            }
+            return [bcPart1, bcPart2, bcPart3, csep, String(vNotZero), "-", String(c2), csep, String(v2NotZero)]
+        } else {
+            if v2 == 999 {
+                return [bcPart1, bcPart2, bcPart3, csep, String(v), "-"]
+            }
+            if reference.isRange {
+                return [bcPart1, bcPart2, bcPart3, csep, String(v), "-", String(v2)]
+            }
+            if v != 0 {
+                return [bcPart1, bcPart2, bcPart3, csep, String(v)]
+            }
+            return [bcPart1, bcPart2, bcPart3]
+        }
+    }
+
+    // MARK: - Operations
+
+    private func isContiguousWith(_ first: BibleReference, other: BibleReference) -> Bool {
+        if first.bookUSFM != other.bookUSFM {
+            return false
+        }
+        if first.bookUSFM == nil {
+            return false
+        }
+        let a, b: BibleReference
+        if BibleReference.compare(a: first, b: other) < 0 {
+            a = first
+            b = other
+        } else {
+            a = other
+            b = first
+        }
+
+        // previously, here, we merged across chapter boundaries.
+        // we can't do that without knowing the # of verses in the chapter in this version.
+        // we don't have that info (well, not yet, not easily).
+
+        if a.chapterEnd < b.chapterStart {
+            return false
+        }
+        if a.chapterEnd > b.chapterStart {
+            return true
+        }
+        if (a.verseEnd + 1) < b.verseStart {
+            return false
+        }
+        return true
+    }
+
+    public func mergeOverlapping(refs: [BibleReference]) -> [BibleReference] {
+        var tmp = refs
+        tmp.sort()
+        var i = 1
+        while i < tmp.count {
+            if isContiguousWith(tmp[i - 1], other: tmp[i]) {
+                tmp[i - 1] = fromOverlappingPair(a: tmp[i - 1], b: tmp[i])
+                tmp.remove(at: i)
+            } else {
+                i += 1
+            }
+        }
+        return tmp
+    }
+
+    nonisolated private func fromOverlappingPair(a: BibleReference, b: BibleReference) -> BibleReference {
+        let x, y: BibleReference
+        guard a.bookUSFM == b.bookUSFM, a.bookUSFM != nil else {  // this shouldn't ever happen; fail in a reasonable way.
+            return a
+        }
+        if BibleReference.compare(a: a, b: b) > 0 {
+            x = b
+            y = a
+        } else {
+            x = a
+            y = b
+        }
+        var further: BibleReference
+        if x.chapterEnd > y.chapterEnd {
+            further = x
+        } else if x.chapterEnd < y.chapterEnd {
+            further = y
+        } else if x.verseEnd > y.verseEnd {
+            further = x
+        } else {
+            further = y
+        }
+        return BibleReference(versionId: x.versionId, bookUSFM: x.bookUSFM!, chapterStart: x.chapterStart, verseStart: x.verseStart, chapterEnd: further.chapterEnd, verseEnd: further.verseEnd)
+    }
+
+    // MARK: - Simple Accessors
+
+    /// e.g. "KJV". Meant to be user-visible.
+    public var abbreviation: String? {
+        if let abbreviation = metadata?.localizedAbbreviation {
+            return abbreviation
+        }
+        return nil
+    }
+
+    public var copyrightLong: String? {
+        if let str = metadata?.copyrightLong {
+            return str.text
+        }
+        return nil
+    }
+
+    public var copyrightShort: String? {
+        if let str = metadata?.copyrightShort {
+            return str.text
+        }
+        return nil
+    }
+
+    public var bookCodes: [String] {
+        metadata?.books.compactMap { $0.usfm } ?? []
+    }
+
+    public func bookName(_ book: String?) -> String? {
+        guard let metadata, let book else {
+            return nil
+        }
+        for b in metadata.books where b.usfm == book {
+            return b.human ?? b.humanLong
+        }
+        return nil
+    }
+
+    /// If metadata hasn't yet been loaded, or if the book code is bad, this will return 0.
+    public func numberOfChaptersInBook(_ bookCode: String?) -> Int {
+        guard let metadata, let bookCode else {
+            return 0
+        }
+        for b in metadata.books where b.usfm == bookCode {
+            if let chapters = b.chapters {
+                return chapters.reduce(0) { $0 + ($1.isCanonical == true ? 1 : 0) }
+            }
+        }
+        return 0
+    }
+
+    /// Returns an array of human-visible labels for chapters.
+    /// In standard English books, this'll be like ["1", "2"...] but other cases exist.
+    /// If metadata hasn't yet been loaded, or if the book code is bad, this will return []
+    public func chapterLabels(_ bookCode: String) -> [String] {
+        guard let metadata else {
+            return []
+        }
+        for b in metadata.books where b.usfm == bookCode {
+            if let chapters = b.chapters {
+                return chapters.compactMap { ($0.isCanonical == true) ? $0.human : nil }
+            }
+        }
+        return []
+    }
+
+}
+
+
+public struct BibleVersionOverview: Decodable, Sendable {
+    public let id: Int
+    public let title: String?
+    public let abbreviation: String?
+    public let language: String?  // iso_639_3
+}
---
+++ swift/URLBuilder.swift
@@ -0,0 +1,88 @@
+public enum URLBuilder {
+    
+    private static var baseURLComponents: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = YouVersionPlatformConfiguration.apiHost
+        return components
+    }
+    
+    static func highlightsURL(usfm: String, versionId: Int, accessToken: String) -> URL? {
+        var components = baseURLComponents
+        components.path = "/highlights/chapter"
+        components.queryItems = [
+            URLQueryItem(name: "lat", value: accessToken),
+            URLQueryItem(name: "version", value: String(versionId)),
+            URLQueryItem(name: "usfm", value: usfm)
+        ]
+        return components.url
+    }
+    
+    static func userURL(accessToken: String) -> URL? {
+        var components = baseURLComponents
+        components.path = "/auth/me"
+        components.queryItems = [
+            URLQueryItem(name: "lat", value: accessToken)
+        ]
+        return components.url
+    }
+    
+    static func authURL(
+        appKey: String,
+        requiredPermissions: Set<YVPPermission> = [],
+        optionalPermissions: Set<YVPPermission> = []
+    ) -> URL? {
+        var components = baseURLComponents
+        components.path = "/auth/login"
+        components.queryItems = [
+            URLQueryItem(name: "app_id", value: appKey),
+            URLQueryItem(name: "language", value: "en"),  // TODO load from the system
+            URLQueryItem(name: "required_perms", value: requiredPermissions.map { $0.rawValue }.joined(separator: ",")),
+            URLQueryItem(name: "opt_perms", value: optionalPermissions.map { $0.rawValue }.joined(separator: ","))
+        ]
+        return components.url
+    }
+    
+    static func votdURL(versionId: Int) -> URL? {
+        var components = baseURLComponents
+        components.path = "/votd/today"
+        components.queryItems = [
+            URLQueryItem(name: "version", value: String(versionId))
+        ]
+        return components.url
+    }
+    
+    static func versionURL(versionId: Int) -> URL? {
+        var components = baseURLComponents
+        components.path = "/bible/version"
+        
+        let env = YouVersionPlatformConfiguration.hostEnv ?? ""
+        components.queryItems = [
+            URLQueryItem(name: "version", value: "\(versionId)\(env)")
+        ]
+        return components.url
+    }
+    
+    static func chapterURL(usfm: String, versionId: Int) -> URL? {
+        var components = baseURLComponents
+        components.path = "/bible/chapter"
+        
+        let env = YouVersionPlatformConfiguration.hostEnv ?? ""
+        components.queryItems = [
+            URLQueryItem(name: "version", value: "\(versionId)"),
+            URLQueryItem(name: "usfm", value: "\(usfm)\(env)")
+        ]
+        return components.url
+    }
+    
+    static func versionsURL(languageTag: String) -> URL? {
+        var components = baseURLComponents
+        components.path = "/bible/versions"
+        
+        let env = YouVersionPlatformConfiguration.hostEnv ?? ""
+        components.queryItems = [
+            URLQueryItem(name: "language", value: "\(languageTag)\(env)")
+        ]
+        return components.url
+    }
+}
